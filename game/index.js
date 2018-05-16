9527d6ba57c1dbf7399d1b9d1bc1dcd855926db2
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-05-16 15:44:02 -0400

    adds game

--- game/index.js
+++ game/index.js
@@ -1,6 +1,64 @@
 import {Map} from 'immutable'
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+const MOVE = `move`
+export const move = (position, player) => {
+	return {
+		type : MOVE,
+		position,
+		player,
+	}
+}
+
+const turnReducer = (turn = `X`, action) => {
+	if(action.type === MOVE){
+		return turn === `X` ? `O` : `X`
+	}else{
+		return turn
+	}
+}
+
+const boardReducer = (board = Map(), action) => {
+	if(action.type === MOVE){
+		return board.setIn(action.player, action.position)
+	}else{
+		return board
+	}
+}
+
+const streak = (board, pos1, pos2, pos3) => {
+	return (
+		board.getIn(pos1) === board.getIn(pos2)
+    && board.getIn(pos1) === board.getIn(pos3)
+    && board.getIn(pos1) !== undefined
+	)
+}
+
+const gameOver = board => {
+	if(streak(board, [0, 0], [0, 1], [0, 2])){return board.getIn([0, 0])}
+	if(streak(board, [1, 0], [1, 1], [1, 2])){return board.getIn([1, 0])}
+	if(streak(board, [2, 0], [2, 1], [2, 2])){return board.getIn([2, 0])}
+	if(streak(board, [0, 0], [1, 0], [2, 0])){return board.getIn([0, 0])}
+	if(streak(board, [0, 1], [1, 1], [2, 1])){return board.getIn([0, 1])}
+	if(streak(board, [0, 2], [1, 2], [2, 2])){return board.getIn([0, 2])}
+	if(streak(board, [0, 0], [1, 1], [2, 2])){return board.getIn([0, 0])}
+	if(streak(board, [0, 2], [1, 1], [2, 0])){return board.getIn([0, 2])}
+	for(let r = 0; r < 3; r++){
+		for(let c = 0; c < 3; c++){
+			if(board.getIn([r, c]) === undefined){
+				return null
+			}
+		}
+	}
+	return `Draw`
+}
+
+export default function reducer(state = {}, action){
+	// TODO
+	const nextBoard = boardReducer(state.board, action)
+	const winnerState = gameOver(nextBoard)
+	return {
+		board : nextBoard,
+		turn : turnReducer(state.turn, action),
+		winner : winnerState,
+	}
 }
--- index.js
+++ index.js
@@ -4,37 +4,48 @@ import gameReducer, {move} from './game'
 import {createStore} from 'redux'
 
 const printBoard = () => {
-  const {board} = game.getState()
-  for (let r = 0; r != 3; ++r) {
-    for (let c = 0; c != 3; ++c) {
-      process.stdout.write(board.getIn([r, c], '_'))
-    }
-    process.stdout.write('\n')
-  }
+	const {board} = game.getState()
+	for(let r = 0; r != 3; ++r){
+		for(let c = 0; c != 3; ++c){
+			process.stdout.write(board.getIn([r, c], `_`))
+		}
+		process.stdout.write(`\n`)
+	}
 }
 
 const getInput = player => async () => {
-  const {turn} = game.getState()  
-  if (turn !== player) return
-  const ans = await inquirer.prompt([{
-    type: 'input',
-    name: 'coord',
-    message: `${turn}'s move (row,col):`
-  }])
-  const [row=0, col=0] = ans.coord.split(/[,\s+]/).map(x => +x)
-  game.dispatch(move(turn, [row, col]))
+	const {turn} = game.getState()
+	if(turn !== player) return
+	const ans = await inquirer.prompt([{
+		type : `input`,
+		name : `coord`,
+		message : `${turn}'s move (row,col):`,
+	}])
+	const [row = 0, col = 0] = ans.coord.split(/[,\s+]/).map(x => +x)
+	game.dispatch(move(turn, [row, col]))
 }
 
 // Create the store
 const game = createStore(gameReducer)
 
 // Debug: Print the state
-// game.subscribe(() => console.log(game.getState()))
+game.subscribe(() => console.log(game.getState()))
 
 game.subscribe(printBoard)
-game.subscribe(getInput('X'))
-game.subscribe(getInput('O'))
+game.subscribe(getInput(`X`))
+game.subscribe(getInput(`O`))
+game.subscribe(() => {
+	if(game.getState().winner === `X` || game.getState().winner === `O`){
+		process.stdout.clearLine()
+		process.stdout.write(`The winner is: ${game.getState().winner}`)
+		process.exit(0)
+	}else if(game.getState().winner === `Draw`){
+		process.stdout.clearLine()
+		process.stdout.write(`Game drawn`)
+		process.exit(0)
+	}
+})
 
 // We dispatch a dummy START action to call all our
 // subscribers the first time.
-game.dispatch({ type: 'START' })
+game.dispatch({ type : `START` })
